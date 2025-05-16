45c366f7f56d11971673c77f1374ef4d7f24cec7
SIGJHWJ <beutti12@gmail.com>
2025-05-16 13:09:54 +0900

    Php

---
+++ 제목없음1111111.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ELISHA</title>
+</head>
+<body>
+<!-- 인트로 시작 -->
+<div style="z-index:1;position:relative;min-width:970px">
+    <div style="position:relative;margin:0 auto;padding:50px 0;width:970px;zoom:1;text-align:center">
+        <p><img src="index.gif" style="max-width:100%; height:auto"></p>
+        <h1>안녕하세요</h1>
+        <h2><a href="index.php">Go Home</a></h2>
+    </div>
+</div>
+<!-- <meta http-equiv='refresh' content='5; url=index.php'> -->
+<!-- } 인트로 끝 -->
+</body>
+</html>
